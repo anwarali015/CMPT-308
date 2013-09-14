5648287f10d7f897db7f9e01847b8bd5a888ed04
anwarali015 <anwar_ali1989@yahoo.com>
2013-09-14 17:50:13 -0400

    Homework Assignment 1

---
+++ queries hw 1.sql
@@ -0,0 +1,51 @@
+﻿-- Name : Mohammed Ali
+-- Date : 09/16/2013
+-- Homework Assignment 1.
+
+-- List all data for all customers --
+select * 
+from customers;
+
+-- List the name and city of agents named Smith. --
+select name, city 
+from agents
+where name = 'Smith';
+
+-- List pid, name, and quantity of products costing more than US$1.25 --
+select pid, name, quantity 
+from products 
+where priceUSD > 1.25
+
+-- List the ordno and aid of all orders --
+select ordno, aid 
+from orders;
+
+-- List the names and cities of customers not in Dallas --
+select name, city 
+from customers
+where city <> 'Dallas';
+ 
+-- List	the names of agents in New York	or Newark --
+select name 
+from agents
+where city = 'New York' OR city = 'Newark';
+
+-- List all data for products not in New York or Newark that cost US$1 or less --
+select * 
+from products 
+where  priceUSD <= 1.00 AND city != 'Newark' and city != 'New York';
+
+-- List	all data for orders in January or March --
+select *
+from orders
+where mon = 'jan' or mon = 'mar';
+
+-- List	all data for orders in February	less than US$100 --
+select * 
+from orders
+where mon = 'feb' AND dollars < 100;
+
+-- List	all orders from	the customer whose cid is C005 --
+select * 
+from orders
+where cid = 'c005';
